6d78d2badac7ec6f3fcddc710b93191d4f50a9c1
Koki Makishima <mr_ver74@yahoo.co.jp>
2021-11-12 13:41:07 +0900

    計画行列のパーツを結合

--- makishima.R
+++ makishima.R
@@ -1,47 +1,56 @@
-#freq <- c(374,602,170,64,18,255,139,71,4,23,42,55,2,6,17,53)
-freq <- c(29, 3, 3, 4, 5, 0, 1, 1, 9, 0, 2, 0, 7, 3, 1, 0)
-#freq <- c(6, 2, 3, 1, 9, 4, 2, 1, 9, 2, 3, 1, 12, 1, 2, 1)
+library(Rsolnp)
+freq <- c(374,602,170,64,18,255,139,71,4,23,42,55,2,6,17,53)
+freq2 <- c(29, 3, 3, 4, 5, 0, 1, 1, 9, 0, 2, 0, 7, 3, 1, 0)
+freq3 <- c(6, 2, 3, 1, 9, 4, 2, 1, 9, 2, 3, 1, 12, 1, 2, 1)
 
-NI <- ifelse(floor(sqrt(length(freq)))
-             <ceiling(sqrt(length(freq))),
-             stop(),sqrt(length(freq)))
-row <- gl(NI,NI,length=NI^2)
-col <- gl(NI,1,length=NI^2)
-u <- c(1:NI)
-sample <- data.frame(freq,row,col)
+# model = function(freq) {
+  NI <- ifelse(floor(sqrt(length(freq)))
+              <ceiling(sqrt(length(freq))),
+              stop(),sqrt(length(freq)))
+  row <- gl(NI,NI,length=NI^2)
+  col <- gl(NI,1,length=NI^2)
+  u <- c(1:NI)
+  sample <- data.frame(freq,row,col)
 
 
-### SI ###
-array_si <- array(0, dim=c(NI^2, (NI-1)))
+  ### SI ###
+  array_si <- array(0, dim=c(NI^2, (NI-1)))
 
-k <- 1
-for (i in 1:NI) {
-  for (j in 1:(NI-1)) {
-    if (i <= (NI-1)) {
-      array_si[k, i] <- array_si[k, i] + 1  
+  k <- 1
+  for (i in 1:NI) {
+    for (j in 1:(NI-1)) {
+      if (i <= (NI-1)) {
+        array_si[k, i] <- array_si[k, i] + 1
+      }
+      if (j <= (NI-1)) {
+        array_si[k, j] <- array_si[k, j] + 1
+      }
+      k <- k + 1
     }
-    if (j <= (NI-1)) {
-      array_si[k, j] <- array_si[k, j] + 1
-    }
-    k <- k + 1
   }
-}
+  
+  array_si <- cbind(rep(1, NI^2), array_si)
 
 
-### SU ###
-array_su <- array(0, dim=c(NI^2, (NI-1)))
+  ### SU ###
+  array_su <- array(0, dim=c(NI^2, (NI-1)))
 
-l <- 1
-for (i in 1:NI) {
-  for (j in 1:(NI-1)) {
-    if (i <= (NI-1)) {
-      array_su[l, i] <- array_su[l, i] + 1  
+  l <- 1
+  for (i in 1:NI) {
+    for (j in 1:(NI-1)) {
+      if (i <= (NI-1)) {
+        array_su[l, i] <- array_su[l, i] + 1
+      }
+      if (j <= (NI-1)) {
+        array_su[l, j] <- array_su[l, j] + 1
+      }
+      l <- l + 1
     }
-    if (j <= (NI-1)) {
-      array_su[l, j] <- array_su[l, j] + 1
-    }
-    l <- l + 1
   }
-}
+  
+  array_su <- cbind(rep(1, NI^2), array_su, c(1:NI %x% 1:NI))
 
-array_su2 <- c(1:NI %x% 1:NI)
+
+  ### Answer ###
+  ans_si <- ans_su <- c()
+# }
