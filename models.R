2f98a690fbc5ee89c2ba448e801b4ac8a10112dc
Koki Makishima <mr_ver74@yahoo.co.jp>
2021-11-15 17:02:25 +0900

    LSQUk

--- models.R
+++ models.R
@@ -4,7 +4,7 @@ freq <- c(29, 3, 3, 4, 5, 0, 1, 1, 9, 0, 2, 0, 7, 3, 1, 0)
 freq2 <- c(374,602,170,64,18,255,139,71,4,23,42,55,2,6,17,53)
 freq3 <- c(6, 2, 3, 1, 9, 4, 2, 1, 9, 2, 3, 1, 12, 1, 2, 1)
 
-# model = function(freq) {
+model = function(freq) {
   NI <- ifelse(floor(sqrt(length(freq)))
               <ceiling(sqrt(length(freq))),
               stop(),sqrt(length(freq)))
@@ -14,6 +14,7 @@ freq3 <- c(6, 2, 3, 1, 9, 4, 2, 1, 9, 2, 3, 1, 12, 1, 2, 1)
   sample <- data.frame(freq,row,col)
 
 
+  
   ##### SI #####
   array_si <- array(0, dim=c(NI^2, (NI-1)))
   k <- 1
@@ -29,24 +30,15 @@ freq3 <- c(6, 2, 3, 1, 9, 4, 2, 1, 9, 2, 3, 1, 12, 1, 2, 1)
     }
   }
   
+  
 
   ##### SU #####
   theta <- c(1:NI %x% 1:NI)
   array_su <- cbind(array_si, theta)
 
 
-  ##### LSQUk #####
-  ## Defines x0, and cs is the vector corresponding to x0.
-  array_cs <- array(0, dim=c(NI,NI))
-  for (i in 1:NI) {
-    for (j in 1:NI) {
-      if (i < j) {
-        array_cs[i, j] <- 1
-      }
-    }
-  }
-  cs <- c(aperm(array_cs))
   
+  ##### LSQUk ver.1(別表現) #####
   ## Defines xl (l = 1, . . . , r −1), 
   ## and f[[l]] is the vector corresponding to xl
   array_f <- array(0, dim=c(NI,NI,NI-1))
@@ -61,35 +53,79 @@ freq3 <- c(6, 2, 3, 1, 9, 4, 2, 1, 9, 2, 3, 1, 12, 1, 2, 1)
   for (k in 1:(NI-1)) {
     f[[k]] <- c(aperm(array_f[,,k]))
   }
-  
 
-  ##### LSQUk #####
-
-  
-
-  ## In a manner similar to Lawal (2001, 2004), 
-  ## the following code defines XS, and s is the matrix corresponding to XS
-  array_s <- array(0, dim=c(1,NI,NI))
-  s <- c()
-  for (i in 1:NI) {
-    for (j in 1:NI) {
+  theta_lsquk <- theta
+  psi <- c()
+  k <- 1
+  for (i in 1:NI){
+    for (j in 1:NI){
       if (i == j) {
-        array_s[1, i, j] <- 1
-        s <- cbind(s, c(array_s[1,,]))
-      } else if (i < j){
-        array_s[1, i, j] <- array_s[1, j, i] <- 1
-        s <- cbind(s, c(array_s[1,,]))
+        theta_lsquk[k] <- 0
+        psi[k] <- 1
+      } else {
+        psi[k] <- 0
       }
-      array_s <- array(0, dim=c(1,NI,NI))
+      k <- k + 1
     }
   }
 
-
+  
+  
+  ##### LSQUk ver.2 #####
+  array_cs <- array(0, dim=c(NI,NI))
+  for (i in 1:NI) {
+    for (j in 1:NI) {
+      if (i < j) {
+        array_cs[i, j] <- 1
+      }
+    }
+  }
+  cs <- c(aperm(array_cs))
+  
+  ## Defines xl (l = 1, . . . , r −1), 
+  ## and f[[l]] is the vector corresponding to xl
+  array_f2 <- array(0, dim=c(NI,NI,NI-1))
+  for (k in 1:(NI-1)) {
+    for (i in 1:NI) {
+      for (j in 1:NI) {
+        array_f2[i, j, k] <- u[j]^k - u[i]^k
+      }
+    }
+  }
+  f2 <- list()
+  for (k in 1:(NI-1)) {
+    f2[[k]] <- c(aperm(array_f[,,k]))
+  }
+  
+  
+  
   ### Answer ###
   m <- list ()
-  m <- append(m, list (SI = glm (freq~array_si, family = poisson, data = sample )))
-  m <- append(m, list (SU = glm (freq~array_su, family = poisson, data = sample )))
+  m <- append(m, list(SI = glm(freq~array_si, family = poisson, data = sample )))
+  m <- append(m, list(SU = glm(freq~array_su, family = poisson, data = sample )))
+  
+  m <- append(m, list(LSQUk_ver1 = glm(freq~array_si+f[[1]]+theta_lsquk+psi, family = poisson, data = sample )))
+  m <- append(m, list(LSQUk_ver1 = glm(freq~array_si+f[[2]]+theta_lsquk+psi, family = poisson, data = sample )))
+  m <- append(m, list(LSQUk_ver1 = glm(freq~array_si+f[[3]]+theta_lsquk+psi, family = poisson, data = sample )))
+  
+  # m <- append(m, list(LSQUk_ver2 = glm(freq~cs+f2[[1]]+theta, family = poisson, data = sample )))
+  # m <- append(m, list(LSQUk_ver2 = glm(freq~cs+f2[[2]]+theta, family = poisson, data = sample )))
+  # m <- append(m, list(LSQUk_ver2 = glm(freq~cs+f2[[3]]+theta, family = poisson, data = sample )))
+
+  # m <- append(m, list(LSQUk_ver1 = glm(freq~array_si+f[[1]]+theta_lsquk+psi, family = poisson, data = sample )))
+  # m <- append(m, list(LSQUk_ver2 = glm(freq~cs+f2[[1]]+theta, family = poisson, data = sample )))
+  # m <- append(m, list(LSQUk_ver1 = glm(freq~array_si+f[[2]]+theta_lsquk+psi, family = poisson, data = sample )))
+  # m <- append(m, list(LSQUk_ver2 = glm(freq~cs+f2[[2]]+theta, family = poisson, data = sample )))
+  # m <- append(m, list(LSQUk_ver1 = glm(freq~array_si+f[[3]]+theta_lsquk+psi, family = poisson, data = sample )))
+  # m <- append(m, list(LSQUk_ver2 = glm(freq~cs+f2[[3]]+theta, family = poisson, data = sample )))
+  
+  #m <- append(m, list(LSQIk = glm(freq~array_si+f[[1]]+psi, family = poisson, data = sample )))
+  #m <- append(m, list(LSQIk = glm(freq~array_si+f[[2]]+psi, family = poisson, data = sample )))
+  #m <- append(m, list(LSQIk = glm(freq~array_si+f[[3]]+psi, family = poisson, data = sample )))
+  
   #m <- append (m, list (SItheta = glm (freq~array_si+theta, family = poisson, data = sample )))
   
-  ans_si <- ans_su <- c()
-# }
+  return (m)
+}
+
+model(freq)
